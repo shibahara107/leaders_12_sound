4342259b2ca3496112c6026d272e8b4432a43566
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-05-11 20:29:19 +0900

    Updates

--- leaders_12_sound/ViewController.swift
+++ leaders_12_sound/ViewController.swift
@@ -12,8 +12,12 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var cymbalButton: UIButton!
     
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    let cymbalSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "symbal")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +33,28 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpDrumButton() {
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownPianoButton() {
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        print("down")
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianoButton() {
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownCymbalButton() {
+        cymbalButton.setImage(UIImage(named: "cymbalPlayingImage"), for: .normal)
+        print("down")
+        cymbalSoundPlayer.currentTime = 0
+        cymbalSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpCymbalButton() {
+        cymbalButton.setImage(UIImage(named: "cymbalImage"), for: .normal)
     }
 
 
